749098e62036c316ee6968facffc1e61b251d6eb
Nikola <nikoladjunisic@gmail.com>
2024-04-09 23:58:34 +0200

    Added transformation for recipes items

--- src/app/components/recipes/recipes.service.ts
+++ src/app/components/recipes/recipes.service.ts
@@ -16,18 +16,37 @@ export class RecipesService {
   fetchRecipes() {
     let url = 'https://660c5f723a0766e85dbe03c7.mockapi.io/recipes';
 
-    this.http.get(url).subscribe(
-      (response: any) => {
-        this.recipesSubject.next(response);
-        this.recipes = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    this.http
+      .get<Recipe[]>(url)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => ({
+            ...recipe,
+            name: this.capitalizeFirstLetter(recipe.name),
+            description: this.trimAndAppendDots(recipe.description),
+          }));
+        })
+      )
+      .subscribe(
+        (response: any) => {
+          this.recipesSubject.next(response);
+          this.recipes = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
   capitalizeFirstLetter(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  trimAndAppendDots(description: string): string {
+    if (description.length > 70) {
+      return description.slice(0, 70) + '...';
+    } else {
+      return description;
+    }
   }
 }
