78391b179fb9066638c9e017b0fe38185b312342
Nikola <nikoladjunisic@gmail.com>
2024-04-10 17:55:54 +0200

    Added filling for filters for categories and authors

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from './recipes.service';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent {}
+export class RecipesComponent implements OnInit {
+  public recipes: Recipe[];
+  categories: string[];
+  authors: string[];
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit(): void {
+    this.recipesService.fetchRecipes().subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+
+        this.categories = Array.from(
+          new Set(recipes.map((recipe) => recipe.category))
+        );
+        this.authors = Array.from(
+          new Set(recipes.map((recipe) => recipe.author))
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  firstUppercase(word: string): string {
+    word = word.toLowerCase();
+    if (!word) return ''; // Ako je reč prazna ili undefined, vratiti prazan string
+
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+}
