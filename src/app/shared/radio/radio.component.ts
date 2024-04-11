4ac416aa250880ac15927602dedf4f1db83909a1
Nikola <nikoladjunisic@gmail.com>
2024-04-12 00:33:52 +0200

    Finished custom radio component

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.scss']
+  styleUrls: ['./radio.component.scss'],
 })
 export class RadioComponent {
-
+  @Input('entity') entity: string;
+  @Input('entityName') entityName: string;
 }
