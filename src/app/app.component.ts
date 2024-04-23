2dac1ff1b472bfee03a04af79144bb6e608313a2
Nikola <nikoladjunisic@gmail.com>
2024-04-23 13:36:04 +0200

    Deleted redudant imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { RecipesService } from './components/recipes/recipes.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
