579e01786ae0143cb59cf34b98a44ae57712156d
Nikola <nikoladjunisic@gmail.com>
2024-05-23 16:10:23 +0200

    Changed IUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './shared/interfaces/i-user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    let user: IUser = {};
+    localStorage.setItem();
+  }
+}
