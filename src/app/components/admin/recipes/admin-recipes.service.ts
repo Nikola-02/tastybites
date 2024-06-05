0b4eea159c0091ec29bf27cf7155730e0108cd37
Nikola <nikoladjunisic@gmail.com>
2024-06-05 08:22:16 +0200

    Added subscibe in dashboard component for recipes

--- src/app/components/admin/recipes/admin-recipes.service.ts
+++ src/app/components/admin/recipes/admin-recipes.service.ts
@@ -1,12 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs-compat/operator/map';
 import { IRecipe } from 'src/app/shared/interfaces/i-recipe';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RecipesService {
+export class AdminRecipesService {
   constructor(private http: HttpClient) {}
 
   getAllRecipesForDashboard() {
--- src/app/components/admin/recipes/dashboard-recipes/dashboard-recipes.component.ts
+++ src/app/components/admin/recipes/dashboard-recipes/dashboard-recipes.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminRecipesService } from '../admin-recipes.service';
+import { IRecipe } from 'src/app/shared/interfaces/i-recipe';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-recipes',
   templateUrl: './dashboard-recipes.component.html',
-  styleUrls: ['./dashboard-recipes.component.scss']
+  styleUrls: ['./dashboard-recipes.component.scss'],
 })
-export class DashboardRecipesComponent {
+export class DashboardRecipesComponent implements OnInit {
+  recipes: IRecipe[] = [];
+  sub: Subscription;
+  constructor(private adminRecipesService: AdminRecipesService) {}
 
+  ngOnInit(): void {
+    this.sub = this.adminRecipesService.getAllRecipesForDashboard().subscribe({
+      next: (response) => {
+        this.recipes = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
